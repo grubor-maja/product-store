9ecbfd3db2bde791ec564a9b27902d5d238431d0
grubor-maja <maja.grubor@outlook.com>
2024-10-03 09:08:16 +0200

    Add database connection setup file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 // const express = require('express') ZASTARELO
 import express from "express";
+import dotenv from "dotenv";
+import {connectDB} from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
+app.get("/", (req,res) => {
+    res.send("Server is ready.")
+})
+
+console.log(process.env.MONGO_URI);
+
 app.listen(5000,() => {
+    connectDB();
     console.log("Server started at port 5000");
 });
 
