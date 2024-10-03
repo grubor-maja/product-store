162ae0e1ca869f899763db284b62a9b1b976d9e6
grubor-maja <maja.grubor@outlook.com>
2024-10-03 17:35:38 +0200

    add deployment files

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,26 @@ import {connectDB} from "./config/db.js";
 import Product from "./models/product.model.js";
 import mongoose from "mongoose";
 import productRoutes from "./routes/product.route.js";
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(express.json()); // allows us to accept JSON data in the req.body
+app.use(express.json()); 
 
-app.get("/", (req,res) => {
-    res.send("Server is ready.")
-});
 
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    })
+}
 
 console.log(process.env.MONGO_URI);
 
