68f5a5590d3c2b01cdcf713206741c1ed0812596
grubor-maja <maja.grubor@outlook.com>
2024-10-03 12:52:12 +0200

    add CRUD endpoints for products

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -12,7 +13,56 @@ app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.get("/", (req,res) => {
     res.send("Server is ready.")
-})
+});
+
+app.get("/api/products",async(req,res) => {
+    try {
+        const products = await Product.find({});
+        if(!products) {
+            return res.status(400).json({success:false,message:"Products not found"});
+        }
+        res.status(201).json({success: true, data: products});
+
+    } catch (error) {
+        res.status(500).json({success: false, message: "Server Error"});
+
+    }
+});
+
+app.get("/api/products/:id",async(req,res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if(!product) {
+            return res.status(400).json({success:false,message:"Product not found"});
+        }
+        res.status(201).json({success: true, data: product});
+
+    } catch (error) {
+        res.status(500).json({success: false, message: "Server Error"});
+
+    }
+});
+
+app.put("/api/products/:id",async(req,res) => {
+
+    const {id} = req.params;
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({success:false, message: "Invalid Product Id"});
+    }
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id,product,{new:true});
+        if(!updatedProduct) {
+            return res.status(400).json({success:false,message:"Product not updated"});
+        }
+        res.status(201).json({success: true, data: updatedProduct});
+
+    } catch (error) {
+        res.status(500).json({success: false, message: "Server Error"});
+
+    }
+});
 
 app.post("/api/products", async(req, res) => {
     const product = req.body; // user will send the data
@@ -33,7 +83,16 @@ app.post("/api/products", async(req, res) => {
     }
 });
 
-app.delete("/api/products/:id")
+app.delete("/api/products/:id", async(req,res) => {
+    const {id} = req.params;
+    // console.log("id: ",id);
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success:true, message:"Product deleted."})
+    } catch (error) {
+        res.status(404).json({success:false,message:"Product not found"});
+    }
+});
 
 console.log(process.env.MONGO_URI);
 
