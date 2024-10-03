67f80be82b1a91060528429497e13b1efe777f59
grubor-maja <maja.grubor@outlook.com>
2024-10-03 12:32:54 +0200

    add model Product

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,44 @@
 import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
 app.get("/", (req,res) => {
     res.send("Server is ready.")
 })
+
+app.post("/api/products", async(req, res) => {
+    const product = req.body; // user will send the data
+
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({success:false,message:"Please provide all fields"});
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({success: true, data: newProduct});
+    } catch (error) {
+        console.error("Error in Create product:",error.message);
+        res.status(500).json({success: false, message: "Server Error"});
+
+    }
+});
+
+app.delete("/api/products/:id")
 
 console.log(process.env.MONGO_URI);
 
 app.listen(5000,() => {
     connectDB();
-    console.log("Server started at port 5000");
+    console.log("Server started at http://localhost:5000");
 });
 
 
